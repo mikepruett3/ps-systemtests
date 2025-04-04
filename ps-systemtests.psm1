da1ceadeced8221073943f2f5b8c98328771f033
Mike Pruett <mikepruett3@gmail.com>
2025-04-04 00:17:35 -0500

    Updated module

--- ps-systemtests.psm1
+++ ps-systemtests.psm1
@@ -1,25 +1,17 @@
-# # Check for WebAdminisration Module
-# If ( !(Get-Module -Name WebAdministration) ) {
-#     Write-Host -ForegroundColor Magenta "WebAdministration Module not loaded!"
-#     Break
-# }
-# # Check for IISAdministration Module
-# If (Get-Module -Name IISAdministration) {
-#     Write-Host -ForegroundColor Red "These cmdlets were designed for the WebAdministration Module only! Will not work with newer IISAdministration Module."
-# }
+#Requires -Modules WebAdminisration, IISAdministration
 
 #Get public and private function definition files. Thanks RamblingCookieMonster
 $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
 #Dot source the files
-Foreach ($import in @($Public + $Private)) {
+Foreach ($Import in @($Public + $Private)) {
     Try {
-        Import-Module $import.fullname
+        Import-Module $Import.FullName
     }
     Catch {
-        Write-Error -Message "Failed to import function $($import.fullname): $_"
+        Write-Error -Message "Failed to import function $($Import.FullName): $_"
     }
 }
 
-Export-ModuleMember -Function $Public.Basename
+Export-ModuleMember -Function $Public.BaseName
